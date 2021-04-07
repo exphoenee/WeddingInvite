e90205331aa70949076d8596e3bdbdd2ff79fae7
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-04-07 14:00:47 +0200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { useRef, useState } from "react";
+
+import MainMenu from "./components/MainMenu";
+
+//import styles
+import "./styles/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <MainMenu />
+    </div>
+  );
+}
+
+export default App;
