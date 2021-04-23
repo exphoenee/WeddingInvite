175770e6e1c008f8a3371f09efce5e9a8a817c8d
exphoenee <61277296+exphoenee@users.noreply.github.com>
2021-04-23 18:41:41 +0200

    DONE!

--- client/src/pages/Live.js
+++ client/src/pages/Live.js
@@ -1,49 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import Iframe from "react-iframe";
 
 import { pageAnimation } from "../animations/animation";
-import Countdown from "../components/Countdown";
-import { WeddingDate } from "../settings";
-import { LiveUrl, test } from "../settings";
+import { LiveUrl } from "../settings";
 
 const Live = () => {
-  const started = () => {
-    const WeddingDateTest = +new Date(`4/12/2021 16:00:00`);
-
-    let CurrentTimeLeft = (test ? WeddingDateTest : WeddingDate) - +new Date();
-    return CurrentTimeLeft / 1000 / 60 / 60 / 24 < 1;
-  };
-
-  const [timeLeft, setTimeLeft] = useState(started());
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(started());
-    }, 1000);
-    // Clear timeout if the component is unmounted
-    return () => clearTimeout(timer);
-  });
-
   return (
     <motion.div
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
-      style={
-        timeLeft
-          ? { height: "100vh", backgroundColor: "black" }
-          : {
-              marginTop: "15%",
-              backgroundColor: "white",
-            }
-      }
+      style={{ height: "100vh", backgroundColor: "black" }}
       className="live"
     >
-      <div style={{ height: "56px" }}></div>
-
-      {timeLeft ? (
+      <div className="media-container">
         <Iframe
           width="100%"
           height="90%"
@@ -54,9 +26,7 @@ const Live = () => {
           allow="fullscreen"
           playing={true}
         />
-      ) : (
-        <Countdown />
-      )}
+      </div>
     </motion.div>
   );
 };
--- client/src/settings.js
+++ client/src/settings.js
@@ -7,7 +7,7 @@ https://www.youtube.com/embed/DDU-rZs-Ic4
 <iframe width="560" height="315" src="https://www.youtube.com/embed/DDU-rZs-Ic4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 */
 
-const videoId = "DDU-rZs-Ic4";
+const videoId = "uCfAAdnUaXU";
 
 export const LiveUrl =
   "https://www.youtube.com/embed/" + videoId + "/?autoplay=1";
@@ -20,4 +20,3 @@ export const WeddingDate = +new Date(`04/24/2021 16:00:00`);
 //* Itt kell átyírni a test-et false-ra és a video ID-t
 //* Majd ide feltölteni:
 //* https://app.netlify.com/sites/adriandviktor/overview
-export const test = false;
